b4ca44166d2cf218003d5505df9d048646fe3cc5
Muhammad Aftab Hameed <adm-muhammad.aftab@orderyoyo.com>
2022-07-30 01:37:45 +0500

    # Refreshing Token

---
+++ src/app/services/token.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  getDecodedAccessToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    } catch(Error) {
+      return null;
+    }
+  }
+}
